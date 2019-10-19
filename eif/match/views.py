b72c25dda2cfca57d108ca6b6c5a471c61982db2
Justin Du <justin.d128@gmail.com>
2019-10-19 01:09:39 -0500

    Add companies, students, student views

--- eif/match/views.py
+++ eif/match/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, match")
+
+def companies(request):
+    companies = Company.objects.order_by('company_name')
+    context = {'companies': companies}
+    return render(request, 'match/companies.html', context)
+
+def student(request, student_id):
+    student = get_object_or_404(Student, pk=student_id)
+    return render(request, 'match/student.html', {'student': student})
+
+def students(request):
+    students = Student.objects.order_by('last_name')
+    context = {'students': students}
+    return render(request, 'match/students.html', context)
