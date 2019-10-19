011bdb38f48f59a9a1d24d0e50301e98f6b435fa
ntheresa <ntheresa@umich.edu>
2019-10-19 05:13:29 -0500

    query

--- eif/match/fetch.py
+++ eif/match/fetch.py
@@ -7,13 +7,19 @@ def connect(path='../db.sqlite3'):
 def get_info(conn):
   c = conn.cursor()
 
-  c.execute('''
+  join_table = c.execute('''
     Select match_student.first_name, match_student.last_name, match_job.name, match_companystudent.rank
     From match_companystudent
     Inner Join match_student On match_companystudent.student_id = match_student.id
     Inner Join match_job On match_companystudent.job_id = match_job.id
+    GROUP BY match_job.name
+    ORDER BY match_companystudent.rank
   ''')
-  print(c.fetchall())
+
+
+
+
+  print(join_table.fetchall().split())
 
 
 get_info(connect())
