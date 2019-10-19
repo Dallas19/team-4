b085a8cac9c5e853543c431f7496f2e9e8a54699
ntheresa <ntheresa@umich.edu>
2019-10-19 01:10:49 -0500

    adding

--- eif/match/urls.py
+++ eif/match/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('/student', views.student, name="student"),
+    path('/company', views.company, name='company')
 ]
--- eif/match/views.py
+++ eif/match/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, match")
+
+def company(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def student(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def job(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
