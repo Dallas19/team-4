4dc006bb5898afe78e7068ce4a01e0e144981a6d
Arami <arami.g@outlook.com>
2019-10-18 21:38:00 -0500

    build the graph with distances

--- matching/matching.py
+++ matching/matching.py
@@ -8,11 +8,38 @@ B = nx.Graph()
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
     
+students = {};
+jobs = {};
+
+# fill the student maps and the jobs map
 with open(os.path.join(__location__, 'students.json')) as json_file:
     data = json.load(json_file)
     for p in data:
-        print('Name: ' + p)
-        print(data[p])
+        students[p] = data[p];
+
+with open(os.path.join(__location__, 'jobs.json')) as json_file:
+    data = json.load(json_file)
+    for p in data:
+        jobs[p] = data[p];
+
+B = nx.Graph()
+
+for key, values in jobs.items():
+    print(key)
+    print(values[0])
+    
+    for i in range(len(values)):
+        for x in range(len(students[values[i]])):
+            if students[values[i]][x] == key:
+                B.add_weighted_edges_from((key, values[i], abs(x-i)))
+                break
+        
+
+    
+
+    
+
+
 
 
 
