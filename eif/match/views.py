baf534caa21ca4ddf11929c9ac0caf8252cef7c6
ntheresa <ntheresa@umich.edu>
2019-10-19 04:01:34 -0500

    fixing data.json keys

--- eif/match/views.py
+++ eif/match/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import *
 
+
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, match")
