eaecdaf581599cecb956133e14726c3a7bbf6499
Justin Du <justin.d128@gmail.com>
2019-10-19 11:00:30 -0500

    Add Student Match Stuff

--- eif/match/urls.py
+++ eif/match/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('students', views.students, name='match-students'),
     path('companies', views.companies, name='match-companies'),
     path('match_for_interviews', views.match_interviews, name='match-match'),
-    path('student_rank', views.student_rank, name='add-student-rank')
+    path('student_rank', views.student_rank, name='add-student-rank'),
+    path('students/add/<int:student_id>', views.student_rank_individual, name='add-student-rank-individual')
 ]
