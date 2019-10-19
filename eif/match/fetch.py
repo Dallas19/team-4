ce2fc3f1d21134de80c15b34eb708159b1f414bb
rex100h <houtanfaridi@gmail.com>
2019-10-19 05:12:37 -0500

    added json parse for db data

--- eif/match/fetch.py
+++ eif/match/fetch.py
@@ -1,19 +1,33 @@
 import sqlite3
+from itertools import groupby
 
 def connect(path='../db.sqlite3'):
   return sqlite3.connect(path)
 
 
-def get_info(conn):
+def get_jobs(conn):
   c = conn.cursor()
 
-  c.execute('''
+  join_table = c.execute('''
     Select match_student.first_name, match_student.last_name, match_job.name, match_companystudent.rank
     From match_companystudent
     Inner Join match_student On match_companystudent.student_id = match_student.id
     Inner Join match_job On match_companystudent.job_id = match_job.id
   ''')
-  print(c.fetchall())
+
+  return parse_jobs(join_table.fetchall())
 
 
-get_info(connect())
+def parse_jobs(jobs):
+  final = {}
+  groups = [(k, v) for k, v in groupby(jobs, key=lambda x: x[2])]
+  for job, info in groups:
+    ordered = sorted(info, key=lambda x: x[3])
+    parsed = list(map(lambda x: x[0] + ' ' + x[1]), ordered)
+    final[job] = parsed
+
+  return final
+
+
+
+get_jobs(connect())
