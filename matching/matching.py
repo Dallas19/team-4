a24d2f44d3af5e65b40c1da77e8f2bb9fb577d3b
Arami <arami.g@outlook.com>
2019-10-19 00:15:58 -0500

    added simple comments for maintainability

--- matching/matching.py
+++ matching/matching.py
@@ -4,11 +4,12 @@ from math import inf
 from itertools import chain
 from heapq import nsmallest
 
-
+# match the students with jobs
 def match(jobs, students, interview_limit):
   B = make_graph(jobs, students)
   return get_matches(B, jobs.keys(), interview_limit)
 
+# create a biparte graph with the student, jobs, and the weight between each node
 def make_graph(jobs, students):
   B = nx.Graph()
   B.add_nodes_from(jobs.keys(), bipartite=0)
@@ -16,7 +17,7 @@ def make_graph(jobs, students):
   B.add_weighted_edges_from( compute_weights(jobs, students) )
   return B
 
-
+# find the weight between each student and jobs
 def compute_weights(jobs, students):
   weights = []
   func = lambda job, student: get_distance(job, student[1], student[0], students)
@@ -26,7 +27,7 @@ def compute_weights(jobs, students):
 
   return list(chain.from_iterable(weights))
 
-
+# find the weight by getting the absolute value of the difference of the indices
 def get_distance(job_id, student_id, rank, students):
   try:
     job_rank = students[student_id].index(job_id)
@@ -35,7 +36,7 @@ def get_distance(job_id, student_id, rank, students):
 
   return (job_id, student_id, abs(rank - job_rank - 1))
 
-
+# return the matches running a minheap to fill the additional positions
 def get_matches(graph, jobs, interview_max):
   matches = {}
   for job in jobs:
